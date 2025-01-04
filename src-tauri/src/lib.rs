f2bb205bbfc09280e426492bdcd99eee2c426ece
Luke Hubbard <luke@screencloud.com>
2025-01-04 22:37:00 +0700

    added autostart

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -4,17 +4,54 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
-use tauri::tray::TrayIconBuilder;
+use tauri::{
+    menu::{Menu, MenuItem},
+    Manager,
+    tray::{TrayIconBuilder, MouseButton, MouseButtonState, TrayIconEvent},
+};
+use tauri_plugin_autostart::{MacosLauncher, ManagerExt};
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        // .plugin(tauri_plugin_autostart::init())
-        // .plugin(tauri_plugin_updater::Builder::new().build())
-        .setup(|app| {  
-            let _tray =TrayIconBuilder::new()
+        .plugin(tauri_plugin_autostart::init(MacosLauncher::LaunchAgent, None))
+        .setup(|app| {
+            #[cfg(desktop)]
+            {
+                let autostart = app.autolaunch();
+                let _ = autostart.enable();
+                if let Ok(is_enabled) = autostart.is_enabled() {
+                    println!("Autostart is {}", if is_enabled { "enabled" } else { "disabled" });
+                }
+            }
+
+            let quit_item = MenuItem::with_id(app, "quit", "Quit", true, None::<&str>)?;
+            let menu = Menu::with_items(app, &[&quit_item])?;
+
+            let _tray = TrayIconBuilder::new()
                 .icon(app.default_window_icon().unwrap().clone())
-                .build(app)?; // TODO: add menu
+                .menu(&menu)
+                .show_menu_on_left_click(true)
+                .on_menu_event(|app, event| match event.id.as_ref() {
+                    "quit" => {
+                        app.exit(0);
+                    }
+                    _ => {}
+                })
+                .on_tray_icon_event(|tray, event| match event {
+                    TrayIconEvent::Click {
+                        button: MouseButton::Left,
+                        button_state: MouseButtonState::Up,
+                        ..
+                    } => {
+                        if let Some(window) = tray.app_handle().get_webview_window("main") {
+                            let _ = window.show();
+                            let _ = window.set_focus();
+                        }
+                    }
+                    _ => {}
+                })
+                .build(app)?;
             Ok(())
         })
         .plugin(tauri_plugin_opener::init())
