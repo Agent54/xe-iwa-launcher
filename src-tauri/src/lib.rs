fe156890aa4a12eda708d69b73b94fdbd1924046
Luke Hubbard <luke@screencloud.com>
2025-01-04 22:22:17 +0700

    added tray icon

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -4,11 +4,22 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+use tauri::tray::TrayIconBuilder;
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        // .plugin(tauri_plugin_autostart::init())
+        // .plugin(tauri_plugin_updater::Builder::new().build())
+        .setup(|app| {  
+            let _tray =TrayIconBuilder::new()
+                .icon(app.default_window_icon().unwrap().clone())
+                .build(app)?; // TODO: add menu
+            Ok(())
+        })
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
+
